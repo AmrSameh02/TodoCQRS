50e87846cffead413b350370970c95c9ebc650f9
Amr.Sameh <amrsameh903@gmail.com>
2026-04-05 22:27:12 +0200

    setup DbContext and Todo configuration for SQLite

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
@@ -13,6 +16,9 @@ namespace API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source = app.db"));
+
 
             var app = builder.Build();
 
---
+++ Infrastructure/Data/Configurations/TodoConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Todos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+	public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+	{
+		public void Configure(EntityTypeBuilder<Todo> builder)
+		{
+			builder.ToTable("Todos");
+			builder.HasKey(t => t.Id);
+			builder.Property(t => t.Title).IsRequired();
+
+		}
+	}
+}
